40083969734a494f2a11c556527154cc046f13be
unknown <daovv@sskpi.com>
2021-09-30 12:15:06 +0700

    main project

---
+++ database/db_connect.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ */
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $db = "quanlykho";
+    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+    //db connect
+?>
